f2e59b5abb5484ec409e9e06fd319d5503fba9af
Clayton Dunwell <clayton.dunwell@gmail.com>
2020-02-20 16:24:41 -0500

    add backend support for filtering by dates

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.core.exceptions import PermissionDenied
 from django.views.generic import CreateView, DeleteView
 from django.views.generic.list import ListView
@@ -15,7 +17,31 @@ class JournalEntryListView(LoginRequiredMixin, ListView):
         return context
 
     def get_queryset(self):
-        return JournalEntry.objects.filter(user=self.request.user).order_by('-pub_date')
+        journal_entries = JournalEntry.objects.filter(user=self.request.user).order_by('-pub_date')
+
+        start_date, end_date = self._get_start_end_date()
+
+        if start_date and not end_date:
+            return journal_entries.filter(pub_date__date__gte=start_date)
+        elif end_date and not start_date:
+            return journal_entries.filter(pub_date__date__lte=end_date)
+        elif start_date and end_date:
+            return journal_entries.filter(pub_date__date__range=(start_date, end_date))
+        else:
+            return journal_entries
+
+    def _get_start_end_date(self):
+        start_date = self.request.GET.get('start_date', None)
+        end_date = self.request.GET.get('end_date', None)
+
+        def get_datetime(date_str):
+            if date_str is not None:
+                try:
+                    return datetime.strptime(date_str, '%m/%d/%Y')
+                except ValueError:
+                    return None
+
+        return get_datetime(start_date), get_datetime(end_date)
 
 
 class JournalEntryCreate(LoginRequiredMixin, CreateView):
